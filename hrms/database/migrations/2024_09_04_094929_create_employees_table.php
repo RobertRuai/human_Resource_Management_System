efb0afbc4bfadd94ac3495214ab7fee9210dcce1
Robert Ruai Mawich <robertruai4@gmail.com>
2024-09-04 13:22:36 +0200

    edit on attributes on employee table

--- hrms/database/migrations/2024_09_04_094929_create_employees_table.php
+++ hrms/database/migrations/2024_09_04_094929_create_employees_table.php
@@ -12,17 +12,32 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
+            $table->id('employee_id');
+            $table->string('employee_id_number');
             $table->string('first_name');
+            $table->string('middle_name');
             $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->foreignId('department_id')->constrained('departments');
-            $table->string('job_title');
-            $table->decimal('salary', 10, 2);
             $table->date('date_of_birth')->nullable();
-            $table->date('date_joined');
-            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->unique();
+            $table->string('city');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('qualification');
+            $table->string('current_experience');
+            $table->string('job_title');
+            $table->string('grade');
+            $table->date('date_of_employment');
+            $table->string('type_of_employment');
+            $table->string('division');
+            $table->string('department');
+            $table->string('location');
+            $table->string('gender');
+            $table->string('marital_status');
+            $table->string('next_of_kin');
+            #$table->foreignId('department_id')->constrained('departments');  
+            #$table->decimal('salary', 10, 2);           
+            #$table->string('photo')->nullable();
             $table->timestamps();
         });
     }
