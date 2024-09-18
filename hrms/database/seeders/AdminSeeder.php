53026c112772ecd324a2136c28f9dfeb8bb0238c
Robert Ruai Mawich <robertruai4@gmail.com>
2024-09-18 13:31:08 +0200

    created Admin and Role database seeders for assigning user roles

---
+++ hrms/database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'username' => 'Admin',
+            'email' => 'admin1@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123456',
+        ]);
+
+        $user->assignRole(['HR Manager', 'Admin' ]);
+    }
+}
